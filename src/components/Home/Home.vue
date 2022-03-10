7949a0d408729ee248d93e69e883a6fdd74d238b
Bappa Dey <bappa.einzigtech@gmail.com>
2022-03-10 15:27:45 +0530

    [WIP] Added bootstrap and home component

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home-page">
+        <h4>Home Page</h4>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'Home'
+    })
+</script>
+
+<style scoped>
+
+</style>
