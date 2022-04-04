b46c2354c6b51a0abfea188a8c2147b47b79ec3e
Bappa Dey <bappa.einzigtech@gmail.com>
2022-04-04 10:39:36 +0530

    [WIP] work need in google map

--- src/components/Home/Products/Maps/Maps.vue
+++ src/components/Home/Products/Maps/Maps.vue
@@ -37,7 +37,9 @@
                 <div class="shadow mt-3">
                     <div class="responsive-map">
                         <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2822.7806761080233!2d-93.29138368446431!3d44.96844997909819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b32b6ee2c87c91%3A0xc20dff2748d2bd92!2sWalker+Art+Center!5e0!3m2!1sen!2sus!4v1514524647889" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
-                        <GMapMap
+                        
+                        
+                        <!-- <GMapMap
                             :center="center"
                             :zoom="10"
                             map-type-id="terrain"
@@ -53,8 +55,9 @@
                                     @click="center = m.position"
                                 />
                             </GMapCluster>
-                        </GMapMap>
-                    
+                        </GMapMap> -->
+
+                        <!-- npm install vue3-google-map -->
                         <!-- <GoogleMap 
                             api-key=""
                             style="width: 100%; height: 110vh" 
@@ -169,7 +172,7 @@
 }
 
 /* Override default vue map style Start */
-.vue-map {
+.vue-map-container .vue-map {
     position: absolute !important;
     overflow: unset !important;
     min-height: 16rem !important;
